afc9c015ae7ab93c2350064245db2321e2aadfc4
aheyhey <68002372+chrispycrust@users.noreply.github.com>
2024-02-29 11:06:03 +1100

    initial commit

---
+++ src/BuildADroid.java
@@ -0,0 +1,33 @@
+
+public class BuildADroid {
+	
+	// instance fields 
+	
+	// constructor
+	
+	public BuildADroid() {
+		
+	}
+	
+	// methods 
+	
+	// energy report
+	
+	// perform task
+	
+	// generate factory code
+	
+	// energy transfer
+	
+	public String toString() {
+		
+		return "";
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
